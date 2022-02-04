3bd02b4235e5624816c3931169316922aa651d60
Hennadii Dobrovolskyi <hdobrovolskyi@envionsoftware.com>
2022-02-04 14:38:29 +0200

    first commit

---
+++ src/main/java/ua/edu/znu/cs/App.java
@@ -0,0 +1,13 @@
+package ua.edu.znu.cs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
