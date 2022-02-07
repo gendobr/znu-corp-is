d89d8c68d71af71853c0502f9b74a2aafb67f5c2
Hennadii Dobrovolskyi <hdobrovolskyi@envionsoftware.com>
2022-02-07 10:41:51 +0200

    updated controller

--- src/main/java/ua/edu/znu/cs/controllers/GreetingController.java
+++ src/main/java/ua/edu/znu/cs/controllers/GreetingController.java
@@ -21,4 +21,17 @@ public class GreetingController {
     Greeting greeting(@RequestParam(defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
+
+        /**
+     * GET http://localhost:8080/add?a=1&b=3
+     * ответ:
+     *    {a:1, b:3, sum:4}
+     */
+    // @GetMapping("/add")
+    // /*TODO: add your type here*/ add(@RequestParam(defaultValue = "0") String a,
+    //              @RequestParam(defaultValue = "0") String b) {
+    //     // return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    //     // TODO: add your code here
+    //     return /*TODO: add your code here to return {a:1, b:3, sum:4}*/;
+    // }
 }
